e1ed6fb8021923096b9d48278d28fc2653d71826
Thomas <hollandpirates@gmail.com>
2018-11-24 20:27:26 +0100

    Update dependencies, IDEA to 2018.3

--- src/com/example/pluginci/HelloKtAction.kt
+++ src/com/example/pluginci/HelloKtAction.kt
@@ -7,8 +7,8 @@ import com.intellij.openapi.ui.Messages
 
 class HelloKtAction : AnAction("Say Hi.") {
 
-    override fun actionPerformed(e: AnActionEvent?) {
-        val project = e?.getData(PlatformDataKeys.PROJECT)
+    override fun actionPerformed(e: AnActionEvent) {
+        val project = e.getData(PlatformDataKeys.PROJECT)
         // Demonstrate using 'generated' files
         val greeting = HelloProvider.getHello()
         // Demonstrate using non-Java/Kotlin files in resources directory
